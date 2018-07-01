1595fca00d0665c6f0ddc5f95cd08e7e5d3175b0
bai <baihuirong111@126.com>
2018-07-01 17:25:16 +0800

    submit

---
+++ Assets/Scripts/BlockManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections; 
+using System.Collections.Generic;
+using UnityEngine;
+using Uniblocks;
+
+public class BlockManager : MonoBehaviour {
+
+	public int range = 10;
+
+	private ushort blockID = 0;
+	private Transform selectedBlockEffect;
+	// Use this for initialization
+	void Start () {
+		selectedBlockEffect = GameObject.Find("selected block graphics").transform;
+		selectedBlockEffect.gameObject.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		SelectBlockID();
+
+		VoxelInfo info = Engine.VoxelRaycast(Camera.main.transform.position, Camera.main.transform.forward, 10, false);
+		if (info != null)
+		{
+			//print(info.index);
+			if( Input.GetMouseButtonDown(0))
+			{
+				Voxel.DestroyBlock(info);
+			}
+			if (Input.GetMouseButtonDown(1))
+			{
+				VoxelInfo newInfo = new VoxelInfo(info.adjacentIndex, info.chunk);
+				Voxel.PlaceBlock(newInfo, blockID);
+			}
+		}
+		UpdateSelectedBlockEffect(info);
+	}
+
+	private void SelectBlockID()
+	{
+		for(ushort i = 1; i < 10; i++)
+		{
+			if (Input.GetKeyDown(i.ToString()))
+			{
+				blockID = i;
+			}
+		}
+	}
+	private void UpdateSelectedBlockEffect(VoxelInfo info)
+	{
+		if (info != null)
+		{
+			selectedBlockEffect.gameObject.SetActive(true);
+			selectedBlockEffect.position = info.chunk.VoxelIndexToPosition(info.index);
+		}
+		else
+		{
+			selectedBlockEffect.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniblocks;
+
+public class MapManager : MonoBehaviour {
+
+	//private bool isGenerate = false;
+	private Transform playerTrans;
+	//private Vector3 lastPosition;
+	private Index lastIndex;
+
+	private void Start()
+	{
+		playerTrans = GameObject.Find ("Player").transform;
+		InvokeRepeating ("InitMap",1,0.5f);
+	}
+
+	private void InitMap()
+	{
+		if (Engine.Initialized == false || ChunkManager.Initialized == false )
+			return;
+		Index currentIndex = Engine.PositionToChunkIndex (playerTrans.position);
+
+		if (lastIndex != currentIndex)
+		{
+			ChunkManager.SpawnChunks (playerTrans.position);
+			lastIndex = currentIndex;
+		}
+
+
+	}
+}
